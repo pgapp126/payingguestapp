38783a1ceb8da48b82eaefc223135f9deaa1549b
pgapp126 <payingguestapplication@gmail.com>
2020-01-06 12:41:24 +0530

    Splashscreen Add

---
+++ app/src/main/java/com/example/pgapp/SplashscreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.pgapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashscreenActivity.this, LoginActivity.class);
+                startActivity(i);
+                        finish();
+            }
+        },4000 );
+
+
+
+
+    }
+}
