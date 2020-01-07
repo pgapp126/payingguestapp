173e06ad7bac122dc5854ff17588e9b8afa0500b
pgapp126 <payingguestapplication@gmail.com>
2020-01-07 11:48:27 +0530

    feedback,mybooking,profile,payment,search activity add

---
+++ app/src/main/java/com/example/pgapp/ViewpaymentActivity.java
@@ -0,0 +1,14 @@
+package com.example.pgapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewpaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpayment);
+    }
+}
