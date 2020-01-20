92dcfd1eb031d716f550fd377c75396d7952cf29
pgapp126 <payingguestapplication@gmail.com>
2020-01-20 12:05:36 +0530

    post pg ad activity add

---
+++ app/src/main/java/com/example/pgapp/PostPgAdActivity.java
@@ -0,0 +1,14 @@
+package com.example.pgapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostPgAdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_pg_ad);
+    }
+}
