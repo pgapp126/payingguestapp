ce13aeeb87551cb7581c0f3eb3778bcd9074e653
pgapp126 <payingguestapplication@gmail.com>
2020-01-21 12:22:08 +0530

    PgAddListActivity

---
+++ app/src/main/java/com/example/pgapp/PgAddListActivity.java
@@ -0,0 +1,14 @@
+package com.example.pgapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PgAddListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pg_add_list);
+    }
+}
--- app/src/main/java/com/example/pgapp/SelectCityActivity.java
+++ app/src/main/java/com/example/pgapp/SelectCityActivity.java
@@ -3,15 +3,72 @@ package com.example.pgapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class SelectCityActivity extends AppCompatActivity {
     EditText edt_searchcity;
+    TextView txt_city;
+    TextView txt_mylocation;
+    ListView lv_cities;
+
+
+    ArrayList<String> citylist=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_city);
+
         edt_searchcity=(EditText)findViewById(R.id.edt_searchcity);
+        txt_city=(TextView) findViewById(R.id.txt_city);
+        txt_mylocation=(TextView) findViewById(R.id.txt_mylocation);
+        lv_cities=(ListView) findViewById(R.id.lv_cities);
+
+
+        citylist.add("Vadodara");
+        citylist.add("Surat");
+        citylist.add("Mumbai");
+        citylist.add("Ahmedabad");
+        citylist.add("Delhi");
+        citylist.add("Banglore");
+        citylist.add("Hyderabad");
+        citylist.add("Pune");
+        citylist.add("Kolkata");
+
+        Collections.sort(citylist,String.CASE_INSENSITIVE_ORDER);
+
+
+        final ArrayAdapter<String> adapter=new ArrayAdapter<>(SelectCityActivity.this,android.R.layout.simple_list_item_1,citylist);
+        lv_cities.setAdapter(adapter);
+
+
+
+        edt_searchcity.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
     }
+
 }
